11fade288aeed6bbed14bcc497079962a57ed37f
dave-projects <dave@theweatherleys.co.uk>
2026-07-27 14:12:56 +0100

    Add motd_server.hpp header

---
+++ include/motd_server.hpp
@@ -0,0 +1,41 @@
+#ifndef MOTD_SERVER_HPP
+#define MOTD_SERVER_HPP
+
+#include <string>
+#include <memory>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/http.hpp>
+#include "logger.hpp"
+
+using boost::asio::ip::tcp;
+namespace ssl = boost::asio::ssl;
+namespace http = boost::beast::http;
+
+class MOTDServer {
+public:
+    MOTDServer(boost::asio::io_context& io_context, int port,
+               const std::string& cert_file, const std::string& key_file,
+               const std::string& motd_file, const std::string& log_file);
+    
+    void start();
+    void stop();
+    
+private:
+    boost::asio::io_context& io_context_;
+    std::unique_ptr<tcp::acceptor> acceptor_;
+    std::shared_ptr<Logger> logger_;
+    std::string motd_file_;
+    std::string current_motd_;
+    
+    ssl::context& get_ssl_context();
+    void accept_connection();
+    void handle_connection(std::shared_ptr<ssl::stream<tcp::socket>> socket);
+    std::string read_motd();
+    void write_motd(const std::string& motd);
+    
+    std::unique_ptr<ssl::context> ssl_context_;
+};
+
+#endif // MOTD_SERVER_HPP
